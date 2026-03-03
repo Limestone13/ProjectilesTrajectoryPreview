1d1e341949506c718a64187088425e4298d0e2dd
Limestone13 <162629508+Limestone13@users.noreply.github.com>
2026-03-03 11:18:38 +0530

    Clean up Ptp class and update initialization log
    Removed unused networking code and updated logger message.

--- src/main/java/fr/madu59/ptp/Ptp.java
+++ src/main/java/fr/madu59/ptp/Ptp.java
@@ -1,14 +1,9 @@
 package fr.madu59.ptp;
 
 import net.fabricmc.api.ModInitializer;
-import net.fabricmc.fabric.api.networking.v1.PayloadTypeRegistry;
-import net.fabricmc.fabric.api.networking.v1.ServerPlayNetworking;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import fr.madu59.ptp.HandshakeNetworking.HANDSHAKE_C2SPayload;
-import fr.madu59.ptp.HandshakeNetworking.HANDSHAKE_S2CPayload;
 
 public class Ptp implements ModInitializer {
 	public static final String MOD_ID = "ptp";
@@ -20,20 +15,6 @@ public class Ptp implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
-
-		PayloadTypeRegistry.playC2S().register(HANDSHAKE_C2SPayload.ID, HANDSHAKE_C2SPayload.CODEC);
-		PayloadTypeRegistry.playS2C().register(HANDSHAKE_S2CPayload.ID, HANDSHAKE_S2CPayload.CODEC);
-
-		ServerPlayNetworking.registerGlobalReceiver(HANDSHAKE_C2SPayload.ID,
-            (payload, context) -> {
-                // Send back a reply packet
-                ServerPlayNetworking.send(context.player(), new HANDSHAKE_S2CPayload("Is installed on server"));
-				LOGGER.info("[PTP] Sending handshake to player...");
-            });
-
-		LOGGER.info("Hello Fabric world!");
+		LOGGER.info("Projectiles Trajectory Preview loaded");
 	}
 }
